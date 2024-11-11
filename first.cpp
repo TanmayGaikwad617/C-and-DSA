b8d9d5884f4b97cbef8577d59e69065d0217b961
TanmayGaikwad617 <tanmaygaikwad617@gmail.com>
2024-11-11 20:56:20 +0530

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+ {    
+      /*
+     cout << "Try programiz.pro";    
+     cout << "Try programiz.pro";
+      */
+cout<<"tanmay";
+
+    return 0;
+}
