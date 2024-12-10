4bb509008c4117b9d72104f66a5e10a067d0950c
TanmayGaikwad617 <tanmaygaikwad617@gmail.com>
2024-12-10 21:28:01 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,1 +1,19 @@
-tejjd
+#include <iostream>
+using namespace std; 
+
+int main()
+ {    
+     int num1,num2,add;
+     cout << "Try programiz.pro"; 
+     
+     cout<<"Enter First Number";
+     cin>>num1;
+     cout<<"Enter Second Number";
+     cin>>num2;
+     add=num1+num2;
+
+     cout << "Addition of two numbers is"<<add; 
+    
+
+  return 0;
+}
