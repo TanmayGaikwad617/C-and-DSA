6649e09ebae7c1cd1d45e4a4aadce0491c7a9ff3
TanmayGaikwad617 <tanmaygaikwad617@gmail.com>
2024-12-10 21:32:24 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -4,16 +4,16 @@ using namespace std;
 int main()
  {    
      int num1,num2,add;
-     cout << "Try programiz.pro"; 
-     
-     cout<<"Enter First Number";
+     //cout << "Try programiz.pro"; 
+
+     cout<<"Enter First Number:";
      cin>>num1;
-     cout<<"Enter Second Number";
+     cout<<"Enter Second Number:";
      cin>>num2;
      add=num1+num2;
 
-     cout << "Addition of two numbers is"<<add; 
-    
+     cout << "Addition of two numbers is "<<add; 
+
 
   return 0;
 }
